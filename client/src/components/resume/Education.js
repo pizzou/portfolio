3ec8a562ab2947d42388dcb8acf1b8e7d2136c5c
pizzou <pmpumuropizzou@gmail.com>
2023-04-14 00:21:44 +0200

    second commit

--- client/src/components/resume/Education.js
+++ client/src/components/resume/Education.js
@@ -18,7 +18,7 @@ const Education = () => {
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="BSc in Computer Science"
-            subTitle="University of DVI (2016 - 2020)"
+            subTitle="University of ? (2016 - 2020)"
             result="3.90/4"
             des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
           />
@@ -28,12 +28,12 @@ const Education = () => {
             result="4.75/5"
             des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
           />
-          {/* <ResumeCard
+          <ResumeCard
             title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
+            subTitle="GS Secondary School (2009 - 2011)"
             result="5.00/5"
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          /> */}
+          />
         </div>
       </div>
       {/* part Two */}
