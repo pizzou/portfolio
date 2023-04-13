e74025161ac748bb3ce432ef7d9af7a1472872e1
pizzou <pmpumuropizzou@gmail.com>
2023-04-13 23:46:45 +0200

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,23 @@
+import logo from "./images/logo.png";
+import bannerImg from "./images/logo.png";
+import projectOne from "./images/projects/projectOne.jpg";
+import projectTwo from "./images/projects/projectTwo.jpg";
+import projectThree from "./images/projects/projectThree.jpeg";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import skills from "./images/testmonial/skills.jpg";
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  testimonialOne,
+  testimonialTwo,
+  quote,
+  skills,
+  contactImg,
+};
---
+++ client/src/components/contact/ChatGPT.js
@@ -0,0 +1,265 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import ContactLeft from './ContactLeft';
+import Title from '../layouts/Title';
+
+const API_KEY = "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";
+
+const systemMessage = { 
+  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+};
+
+
+const ChatGPT = () => {
+
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm ChatGPT!from GMS GROUP Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
+
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) { 
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+
+    
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage, 
+        ...apiMessages 
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data.choices[0].message.content);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
+  }
+  // const [username, setUsername] = useState("");
+  // const [phoneNumber, setPhoneNumber] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [subject, setSubject] = useState("");
+  // const [message, setMessage] = useState("");
+  // const [errMsg, setErrMsg] = useState("");
+  // const [successMsg, setSuccessMsg] = useState("");
+
+  
+  // const emailValidation = () => {
+  //   return String(email)
+  //     .toLocaleLowerCase()
+  //     .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  // };
+ 
+
+  // const handleSend = (e) => {
+  //   e.preventDefault();
+  //   if (username === "") {
+  //     setErrMsg("Username is required!");
+  //   } else if (phoneNumber === "") {
+  //     setErrMsg("Phone number is required!");
+  //   } else if (email === "") {
+  //     setErrMsg("Please give your Email!");
+  //   } else if (!emailValidation(email)) {
+  //     setErrMsg("Give a valid Email!");
+  //   } else if (subject === "") {
+  //     setErrMsg("Plese give your Subject!");
+  //   } else if (message === "") {
+  //     setErrMsg("Message is required!");
+  //   } else {
+  //     setSuccessMsg(
+  //       `Thank you dear ${username}, Your Messages has been sent Successfully!`
+  //     );
+  //     setErrMsg("");
+  //     setUsername("");
+  //     setPhoneNumber("");
+  //     setEmail("");
+  //     setSubject("");
+  //     setMessage("");
+  //   }
+  // };
+  return (
+    <section
+      id="contact"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title title="CONTACT" des="Contact With Me" />
+      </div>
+      <div className="w-full">
+        <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
+          <ContactLeft />
+          <div className="App">
+      <div style={{ position:"relative", height: "300px", width: "700px", backgroundColor:"black" }}>
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+
+
+{/* <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
+            <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
+              {errMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {errMsg}
+                </p>
+              )}
+              {successMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {successMsg}
+                </p>
+              )}
+              <div className="w-full flex flex-col lgl:flex-row gap-10">
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
+                    Your name
+                  </p>
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    className={`${
+                      errMsg === "Username is required!" &&
+                      "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+                <div className="w-full lgl:w-1/2 flex flex-col gap-4">
+                  <p className="text-sm text-gray-400 uppercase tracking-wide">
+                    Phone Number
+                  </p>
+                  <input
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    value={phoneNumber}
+                    className={`${
+                      errMsg === "Phone number is required!" &&
+                      "outline-designColor"
+                    } contactInput`}
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Email
+                </p>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  className={`${
+                    errMsg === "Please give your Email!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="email"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Subject
+                </p>
+                <input
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                  className={`${
+                    errMsg === "Plese give your Subject!" &&
+                    "outline-designColor"
+                  } contactInput`}
+                  type="text"
+                />
+              </div>
+              <div className="flex flex-col gap-4">
+                <p className="text-sm text-gray-400 uppercase tracking-wide">
+                  Message
+                </p>
+                <textarea
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                  className={`${
+                    errMsg === "Message is required!" && "outline-designColor"
+                  } contactTextArea`}
+                  cols="30"
+                  rows="8"
+                ></textarea>
+              </div>
+              <div className="w-full">
+                <button
+                  onClick={handleSend}
+                  className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
+                >
+                  Send Message
+                </button>
+              </div>
+              {errMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
+                  {errMsg}
+                </p>
+              )}
+              {successMsg && (
+                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
+                  {successMsg}
+                </p>
+              )}
+            </form>
+          </div> */}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ChatGPT
